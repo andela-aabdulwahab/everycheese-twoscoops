a340ae215aba18044f07eb67966f98aead212fa8
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2016-10-18 12:42:34 +0100

    [Feature] Create the cheese app

---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from autoslug import AutoSlugField
+
+
+class Cheese(models.Model):
+
+    FIRMNESS_UNSPECIFIED = 'unspecified'
+    FIRMNESS_SOFT = 'soft'
+    FIRMNESS_SEMI_SOFT = 'semi-soft'
+    FIRMNESS_SEMI_HARD = 'semi-hard'
+    FIRMNESS_HARD = 'hard'
+
+    FIRMNESS_CHOICES = (
+        (FIRMNESS_UNSPECIFIED, 'Unspecified'),
+        (FIRMNESS_SOFT, 'Soft'),
+        (FIRMNESS_SEMI_SOFT, 'Semi-Soft'),
+        (FIRMNESS_SEMI_HARD, 'Semi-Hard'),
+        (FIRMNESS_HARD, 'Hard'),
+    )
+
+    name = models.CharField("Name of cheese", max_length=255)
+    slug = AutoSlugField("Cheese Address", unique=True, always_update=False,
+                         populate_from='name')
+    description = models.TextField("Description of Cheese", blank=True)
+    firmness = models.CharField("Firmness", choices=FIRMNESS_CHOICES,
+                                default=FIRMNESS_UNSPECIFIED, max_length=20)
+
+    def __str__(self):
+        return self.name
