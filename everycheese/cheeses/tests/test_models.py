0cc58f750dd45dba426162384fd30cb008595d4a
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2016-10-20 10:36:29 +0100

    [Chore] Create Cheese factory

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,13 +1,9 @@
 from test_plus.test import TestCase
-from everycheese.cheeses.models import Cheese
+from .factories import CheeseFactory
 
 
 class TestCheese(TestCase):
 
     def test__str__(self):
-        cheese = Cheese.objects.create(
-            name="Stracchino",
-            description="semi-sweet cheese that goes along well with starches",
-            firmness=Cheese.FIRMNESS_SEMI_SOFT
-        )
-        self.assertEqual(cheese.__str__(), 'Stracchino')
+        cheese = CheeseFactory()
+        self.assertEqual(cheese.__str__(), cheese.name)
